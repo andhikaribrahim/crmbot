04a03ff02118de75cb388666f5b1c745a6ac01c7
Andhika R Ibrahim <andhikaribrahim@gmail.com>
2021-09-08 23:11:45 +0700

    Finishing up development environment

--- app.js
+++ app.js
@@ -1,13 +1,47 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const fs = require('fs');
+const https = require('https');
+const path = require('path');
+const express = require('express');
+const axios = require('axios');
+const TelegramBot = require('node-telegram-bot-api');
+const port = 3000;
 
-require('dotenv').config()
+require('dotenv').config({
+  path: process.env.NODE_ENV === 'development'
+    ? path.resolve(__dirname, './.env')
+    : path.resolve(__dirname, `./.env.${process.env.NODE_ENV}`)
+});
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+const key = fs.readFileSync(path.resolve(__dirname, './certs/key.pem'));
+const cert = fs.readFileSync(path.resolve(__dirname, './certs/cert.pem'));
 
-app.listen(port, () => {
-  console.log(`Example app listening at http://localhost:${port}`)
-})
+const token = process.env.TOKEN;
+const bot = new TelegramBot(token, { polling: true });
+
+
+const app = express();
+
+const server = https.createServer({ key: key, cert: cert }, app);
+
+app.use(express.json());
+
+app.post('/bot/message', async (req, res) => {
+  bot.sendMessage(process.env.GROUP_ID, 'hello world');
+  res.sendStatus(200);
+});
+
+app.get(`/bot/updates`, async (req, res) => {
+  const updates = await axios(process.env.TELEGRAM_URL + token + '/getUpdates')
+    .then(r => r?.data?.result);
+  console.log(JSON.stringify(updates));
+  res.sendStatus(200);
+});
+
+server.listen(port, () => {
+  console.log(`listening on ${port}`);
+});
+
+/* Ping bot */
+bot.on('message', context => {
+  bot.sendMessage(context.chat.id, 'Hello sir!');
+});
