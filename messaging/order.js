1de9eaabc34b937d08a9025681c858bcaf284325
Andhika R Ibrahim <andhikaribrahim@gmail.com>
2021-09-11 00:21:27 +0700

    Add title

--- messaging/order.js
+++ messaging/order.js
@@ -1,12 +1,13 @@
 const phoneParser = require('../helpers/phoneParser');
 
 function orderMessage(body) {
-  const { whatsapp, phone, name } = body;
+  const { whatsapp, phone, name, title } = body;
   return `
 NEW ORDER COMING UP!\n
 Name: ${name}\n
 Whatsapp: <a href="https://wa.me/${phoneParser(whatsapp)}">${whatsapp}</a>\n
 Phone: <a href="tel:+${phoneParser(phone)}">${phone}</a>\n
+Produk: ${title}
   `;
 };
 
